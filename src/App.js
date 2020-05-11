265b73f6c0af36f539ccb910fa5982c79d08581b
Selena <selenawalshsmith@gmail.com>
2020-05-11 14:35:35 -0700

    x

--- src/App.js
+++ src/App.js
@@ -80,18 +80,6 @@ class App extends Component {
     )
   }
 }
-export const flattenObject = (obj) => {
-  const flattened = {}
 
-  Object.keys(obj).forEach((key) => {
-    if (typeof obj[key] === 'object' && obj[key] !== null) {
-      Object.assign(flattened, flattenObject(obj[key]))
-    } else {
-      flattened[key] = obj[key]
-    }
-  })
-
-  return flattened
-}
 
 export default App;
