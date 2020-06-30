e4e907a7e0f75391749a32c444fc825e1fe8eb46
Selena <selenawalshsmith@gmail.com>
2020-06-30 16:44:36 -0700

    configuring heroku

--- server.js
+++ server.js
@@ -46,3 +46,14 @@ app.use("/api/users", users);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
