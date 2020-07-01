30374311cd9219ec2c3780da7cd40789abc195f7
Selena <selenawalshsmith@gmail.com>
2020-06-30 22:15:48 -0700

    config server.js for heroku

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use("/api/users", users);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
-
+/*
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
   app.use(express.static('client/build'));
@@ -57,3 +57,13 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+*/
+app.use(express.static(path.join(__dirname, './client/public')))
+
+app.get('*', function(_, res) {
+  res.sendFile(path.join(__dirname, './client/public/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
